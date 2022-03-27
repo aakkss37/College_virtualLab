c605942356b598365f2497c3fdc78631522312d1
amar kumar <amarkumar.sharma.124@gmail.com>
2022-03-27 10:33:11 +0530

    added department lab landing page and made it templet of all departments

--- app.js
+++ app.js
@@ -12,6 +12,44 @@ app.get('/home', (req, resp) => {
     resp.render('home')
 })
 
+
+app.get('/home/:departmantName', (req, resp) => {
+    let departmentRequested = req.params.departmantName;
+    console.log(departmentRequested)
+
+    if (departmentRequested === "biotechnologylab") {
+        resp.render('departmentLab', {
+            DepartmentName: "Biotechnology and Biomedical Engineering"
+        })
+    }
+    else if (departmentRequested === "chemistrylab") {
+        resp.render('departmentLab', {
+            DepartmentName: 'Chemical Science'
+        })
+    }
+    else if (departmentRequested === "computersciencelab") {
+        resp.render('departmentLab', {
+            DepartmentName: 'Computer Science'
+        })
+    }
+    else if (departmentRequested === "mechanicallab") {
+        resp.render('departmentLab', {
+            DepartmentName: 'Mechanical Engeneering'
+        })
+    }
+    else if (departmentRequested === "physicslab") {
+        resp.render('departmentLab', {
+            DepartmentName: "Physics Lab"
+        })
+    }
+    else if (departmentRequested === "cybersecuritylab") {
+        resp.render('departmentLab', {
+            DepartmentName: 'Cyber Security'
+        })
+    }
+
+})
+
 app.listen(3000, () => {
     console.log('Server Running On PORT 3000')
 })
