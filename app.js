db324d48169da23ad991d5a7f32fcb6c7e1fc1b1
amar kumar <amarkumar.sharma.124@gmail.com>
2022-04-03 09:00:19 +0530

    connected to mongoose atlas

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require("ejs");
-
+const mongoose = require('mongoose')
+const _ = require('lodash')
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+mongoose.connect = "mongodb+srv://admin:admin123@cluster0.khkmp.mongodb.net/karpagam_virtual_lab";
+
+
 
 // ! Student Side
 
